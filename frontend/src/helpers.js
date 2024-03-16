6f3ace8408075f423024a1579cd14fc969afd50b
mohammadarsalah <m.arsalah003@gmail.com>
2024-03-17 00:45:33 +1100

    up to comments logic

--- frontend/src/helpers.js
+++ frontend/src/helpers.js
@@ -6,26 +6,114 @@
  *   https://developer.mozilla.org/en-US/docs/Web/API/File
  *   https://developer.mozilla.org/en-US/docs/Web/API/FileReader
  *   https://developer.mozilla.org/en-US/docs/Web/HTTP/Basics_of_HTTP/Data_URIs
- * 
+ *
  * Example Usage:
  *   const file = document.querySelector('input[type="file"]').files[0];
  *   console.log(fileToDataUrl(file));
  * @param {File} file The file to be read.
  * @return {Promise<string>} Promise which resolves to the file as a data url.
  */
-export function fileToDataUrl(file) {
-    const validFileTypes = [ 'image/jpeg', 'image/png', 'image/jpg' ]
-    const valid = validFileTypes.find(type => type === file.type);
-    // Bad data, let's walk away.
-    if (!valid) {
-        throw Error('provided file is not a png, jpg or jpeg image.');
-    }
-    
-    const reader = new FileReader();
-    const dataUrlPromise = new Promise((resolve,reject) => {
-        reader.onerror = reject;
-        reader.onload = () => resolve(reader.result);
-    });
-    reader.readAsDataURL(file);
-    return dataUrlPromise;
+function fileToDataUrl(file) {
+  const validFileTypes = ["image/jpeg", "image/png", "image/jpg"];
+  const valid = validFileTypes.find((type) => type === file.type);
+  // Bad data, let's walk away.
+  if (!valid) {
+    throw Error("provided file is not a png, jpg or jpeg image.");
+  }
+
+  const reader = new FileReader();
+  const dataUrlPromise = new Promise((resolve, reject) => {
+    reader.onerror = reject;
+    reader.onload = () => resolve(reader.result);
+  });
+  reader.readAsDataURL(file);
+  return dataUrlPromise;
 }
+
+const getRelativeTimeString = (date, lang = navigator.language) => {
+  // Allow dates or times to be passed
+  const timeMs = typeof date === "number" ? date : date.getTime();
+
+  // Get the amount of seconds between the given date and now
+  const deltaSeconds = Math.round((timeMs - Date.now()) / 1000);
+
+  // Array reprsenting one minute, hour, day, week, month, etc in seconds
+  const cutoffs = [
+    60,
+    3600,
+    86400,
+    86400 * 7,
+    86400 * 30,
+    86400 * 365,
+    Infinity,
+  ];
+
+  // Array equivalent to the above but in the string representation of the units
+  const units = ["second", "minute", "hour", "day", "week", "month", "year"];
+
+  // Grab the ideal cutoff unit
+  const unitIndex = cutoffs.findIndex(
+    (cutoff) => cutoff > Math.abs(deltaSeconds)
+  );
+
+  // Get the divisor to divide from the seconds. E.g. if our unit is "day" our divisor
+  // is one day in seconds, so we can divide our seconds by this to get the # of days
+  const divisor = unitIndex ? cutoffs[unitIndex - 1] : 1;
+
+  // Intl.RelativeTimeFormat do its magic
+  const rtf = new Intl.RelativeTimeFormat(lang, { numeric: "auto" });
+  return rtf.format(Math.floor(deltaSeconds / divisor), units[unitIndex]);
+};
+const request = (path, method, body, token) =>
+  fetch(`http://localhost:5005/` + path, {
+    method,
+    headers: {
+      "Content-type": "application/json",
+      Authorization: `Bearer ${token}`,
+    },
+    ...(body && { body: JSON.stringify(body) }),
+  })
+    .catch((err) => displayError(err))
+    .then((res) => {
+      try {
+        return res.json();
+      } catch (error) {
+        return Promise.reject(error);
+      }
+    })
+    .then((data) =>
+      data.error ? Promise.reject(data.error) : Promise.resolve(data)
+    );
+
+const setToken = (token) =>
+  localStorage.setItem("token", JSON.stringify(token));
+
+const getToken = () => JSON.parse(localStorage.getItem("token")).token;
+
+const getUserId = () => JSON.parse(localStorage.getItem("token")).userId;
+const removeToken = () => localStorage.removeItem("token");
+
+const displayError = (err) => {
+  document.querySelector("#err-model").querySelector("p").innerText = err;
+  document.querySelector("#err-model").style.display = "block";
+};
+
+const displayNav = (navType) => {
+  document
+    .querySelectorAll("nav")
+    .forEach((nav) => (nav.style.display = "none"));
+
+  document.querySelector(`#${navType}`).style.display = "block";
+};
+
+export {
+  fileToDataUrl,
+  getRelativeTimeString,
+  request,
+  setToken,
+  displayError,
+  getToken,
+  getUserId,
+  removeToken,
+  displayNav,
+};
--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,5 +1,470 @@
-import { BACKEND_PORT } from './config.js';
+import { BACKEND_PORT } from "./config.js";
 // A helper you may want to use when uploading new images to the server.
-import { fileToDataUrl } from './helpers.js';
+import {
+  fileToDataUrl,
+  getRelativeTimeString,
+  request,
+  displayError,
+  getToken,
+  getUserId,
+  removeToken,
+  setToken,
+  displayNav,
+} from "./helpers.js";
 
-console.log('Let\'s go!');
+const pages = ["login", "register", "dashboard"];
+const subpages = ["create-thread"];
+let intervalId;
+let page = 0;
+/*
+    Helper functions
+*/
+const goToPage = (page) => {
+  if (["login", "register"].includes(page)) {
+    displayNav("nav");
+    clearInterval(intervalId);
+    document.querySelector(".header-user-name").innerText = "";
+  } else {
+    displayNav("auth-nav");
+  }
+
+  if (page === "dashboard") return goToSubpage("dashboard");
+  document
+    .querySelectorAll("main > .page")
+    .forEach((page) => (page.style.display = "none"));
+  document.querySelector(`#${page}-page`).style.display =
+    page === "auth" ? "flex" : "block";
+};
+
+const goToSubpage = (subpage) => {
+  request(`user?userId=${getUserId()}`, "GET", "", token).then(
+    ({ email }) =>
+      (document.querySelector(".header-user-name").innerText = email)
+  );
+  displayThreads(page);
+  clearInterval(intervalId);
+  intervalId = setInterval(() => {
+    displayThreads(page);
+  }, 1000 * 30);
+  goToPage("auth");
+  document
+    .querySelectorAll("#main-content > .subpage")
+    .forEach((subpage) => (subpage.style.display = "none"));
+
+  document.querySelector(`#${subpage}-subpage`).style.display = "flex";
+};
+const displayThreads = (start) => {
+  document.querySelector("#thread-list").innerHTML = "";
+
+  return new Promise((resolve) =>
+    request(`threads?start=${start}`, "GET", "", token).then((data) => {
+      if (data.length === 0) resolve("No threads left to display");
+      const threadIdPromises = [];
+      for (const threadId of data) {
+        threadIdPromises.push(
+          request(`thread?id=${threadId}`, "GET", "", token)
+        );
+      }
+
+      Promise.all(threadIdPromises).then((data) =>
+        data.forEach(({ creatorId, title, createdAt, likes, id }) => {
+          request(`user?userId=${creatorId} `, "GET", "", token)
+            .then(({ name }) => {
+              const card = document
+                .querySelector(".thread-card")
+                .cloneNode(true);
+              card.style.display = "block";
+              card.querySelector(".card-header").innerText = title;
+              card.querySelector(".card-title").innerText = name;
+              card.querySelector(".card-id").innerText = id;
+              request(`user?userId=${creatorId}`, "GET", "", token).then(
+                ({ email }) =>
+                  (card.querySelector(".card-creator-name").innerText = email)
+              );
+
+              card.querySelector(".card-text").innerText = `${createdAt}| ${
+                likes.length
+              } like${likes.length === 1 ? "" : "s"} `;
+              document.querySelector("#thread-list").appendChild(card);
+
+              resolve("Success");
+              card.addEventListener("click", (e) => {
+                request(`thread?id=${id}`, "GET", "", token).then(
+                  ({ title, content, likes, id }) => {
+                    viewThread(id, getUserId());
+                  }
+                );
+              });
+            })
+
+            .then(() => {
+              const list = document.querySelector("#thread-list");
+              [...list.children]
+                .sort((a, b) => {
+                  const date1 = new Date(
+                    a.querySelector(".card-text").innerText.split("|")[0]
+                  );
+
+                  const date2 = new Date(
+                    b.querySelector(".card-text").innerText.split("|")[0]
+                  );
+                  return date2.getTime() - date1.getTime();
+                })
+                .forEach((node) => list.appendChild(node));
+            });
+        })
+      );
+    })
+  );
+};
+
+const viewThread = (threadId, userId) =>
+  request(`thread?id=${threadId}`, "GET", "", token).then(
+    ({ title, content, isPublic, lock, likes, creatorId, id, watchees }) => {
+      if (likes.includes(userId)) {
+        document.querySelector("#like-thread-btn").style.background = "red";
+      } else {
+        document.querySelector("#like-thread-btn").style.background = "";
+      }
+
+      if (watchees.includes(userId)) {
+        document.querySelector("#watch-thread-btn").style.background = "red";
+      } else {
+        document.querySelector("#watch-thread-btn").style.background = "";
+      }
+      document.querySelector("#comments").innerHTML = "";
+      request(`comments?threadId=${threadId}`, "GET", "", token).then(
+        (comments) =>
+          comments
+            .filter(({ parentCommentId }) => parentCommentId === null)
+            .forEach(
+              ({
+                content,
+                createdAt,
+                likes,
+                parentCommentId,
+                creatorId,
+                id,
+              }) => {
+                const card = document
+                  .querySelector(".comment-thread-container")
+                  .cloneNode(true);
+
+                card.style.display = "block";
+
+                card.querySelector(
+                  ".card-title"
+                ).innerText = `replied to ${parentCommentId}`;
+
+                card.querySelector(".card-id").innerText = ` comment id: ${id}`;
+
+                card.addEventListener("click", (e) => {
+                  const parentContainer = e.target.closest(
+                    ".comment-thread-container"
+                  );
+
+                  const commentReplyingTo = e.target.closest(".thread-card");
+
+                  const commentId = commentReplyingTo
+                    .querySelector(".card-id")
+                    .innerText.split(" ")[2];
+
+                  request(
+                    `comments?threadId=${threadId}`,
+                    "GET",
+                    "",
+                    token
+                  ).then((comments) => {
+                    const replies = comments.filter(
+                      (comment) => comment.parentCommentId == commentId
+                    );
+                    console.log(
+                      parentContainer.childElementCount,
+                      replies.length
+                    );
+                    if (
+                      parentContainer.childElementCount - 1 ===
+                      replies.length
+                    ) {
+                      return;
+                    }
+                    replies.forEach(
+                      ({
+                        title,
+                        content,
+                        isPublic,
+                        lock,
+                        likes,
+                        creatorId,
+                        id,
+                        watchees,
+                        parentCommentId,
+                      }) => {
+                        const card = document
+                          .querySelector(".comment-thread-container")
+                          .cloneNode(true);
+
+                        card.style.display = "block";
+                        const parentContainerMargin = parseInt(
+                          getComputedStyle(parentContainer).marginLeft
+                        );
+                        console.log(parentContainerMargin);
+                        card.style.marginLeft =
+                          parentContainerMargin + 30 + "px";
+
+                        card.querySelector(
+                          ".card-title"
+                        ).innerText = `replied to ${parentCommentId}`;
+
+                        card.querySelector(
+                          ".card-id"
+                        ).innerText = ` comment id: ${id}`;
+
+                        request(
+                          `user?userId=${creatorId}`,
+                          "GET",
+                          "",
+                          token
+                        ).then(({ email, image }) => {
+                          card.querySelector(".card-creator-name").innerText =
+                            email;
+                          card.querySelector("#card-img").src = image;
+                        });
+                        const rtf = new Intl.RelativeTimeFormat("en", {
+                          numeric: "auto",
+                        });
+                        const dateFromNow = getRelativeTimeString(
+                          new Date(createdAt)
+                        );
+                        card.querySelector(
+                          ".card-text"
+                        ).innerText = `content: ${content}, this comment was created: ${dateFromNow} `;
+
+                        card.querySelector(".card-body").style.flexDirection =
+                          "column";
+
+                        parentContainer.appendChild(card);
+                      }
+                    );
+                  });
+                });
+
+                request(`user?userId=${creatorId}`, "GET", "", token).then(
+                  ({ email, image }) => {
+                    card.querySelector(".card-creator-name").innerText = email;
+                    card.querySelector("#card-img").src = image;
+                  }
+                );
+                const rtf = new Intl.RelativeTimeFormat("en", {
+                  numeric: "auto",
+                });
+                const dateFromNow = getRelativeTimeString(new Date(createdAt));
+                card.querySelector(
+                  ".card-text"
+                ).innerText = `content: ${content}, this comment was created: ${dateFromNow} `;
+
+                card.querySelector(".card-body").style.flexDirection = "column";
+
+                // card.removeChild(card.querySelector(".card-header"));
+                document.querySelector("#comments").appendChild(card);
+              }
+            )
+      );
+
+      document.querySelector(".view-thread-title").innerText = title;
+      document.querySelector(".view-thread-creator").innerText = creatorId;
+
+      document.querySelector(".view-thread-content").innerText = content;
+      document.querySelector(
+        ".view-thread-likes"
+      ).innerText = `${likes.length}`;
+      document.querySelector("#edit-thread-btn").style.display =
+        creatorId === getUserId() ? "flex" : "none";
+
+      document.querySelector("#delete-thread-btn").style.display =
+        creatorId === getUserId() ? "flex" : "none";
+
+      document.querySelector(".view-thread-id").innerText = id;
+      goToSubpage("view-thread");
+    }
+  );
+
+/*
+    Main program
+*/
+let token = JSON.parse(localStorage.getItem("token"))?.token;
+
+goToPage(token ? "dashboard" : "login");
+
+document.querySelectorAll("header .normal-link").forEach((link) =>
+  link.addEventListener("click", (e) => {
+    e.preventDefault();
+    const page = link.id.split("-")[0];
+
+    goToPage(page);
+  })
+);
+
+document.querySelector("#login-btn").addEventListener("click", (e) => {
+  e.preventDefault();
+  const email = document.querySelector("#login-email").value;
+  const password = document.querySelector("#login-password").value;
+
+  request("auth/login", "POST", { email, password })
+    .then((data) => {
+      setToken(data);
+      token = getToken();
+
+      goToPage("dashboard");
+    })
+    .catch((err) => {
+      displayError(err);
+    });
+});
+
+document.querySelector("#register-btn").addEventListener("click", (e) => {
+  e.preventDefault();
+  const email = document.querySelector("#register-email").value;
+  const name = document.querySelector("#register-name").value;
+  const password = document.querySelector("#register-password").value;
+  const passwordConfirm = document.querySelector(
+    "#register-password-confirm"
+  ).value;
+
+  if (password !== passwordConfirm)
+    return displayError("Passwords dont match!");
+  request("auth/register", "POST", {
+    email,
+    password,
+    name,
+  })
+    .then((data) => {
+      setToken(data);
+      token = getToken();
+      goToPage("dashboard");
+    })
+
+    .catch((err) => {
+      displayError(err);
+    });
+});
+
+document
+  .querySelector("div.modal .close")
+  .addEventListener(
+    "click",
+    ({ target }) => (document.querySelector("div.modal").style.display = "none")
+  );
+
+document.querySelector("#logout-link").addEventListener("click", (e) => {
+  e.preventDefault();
+  removeToken();
+  token = null;
+  goToPage("login");
+});
+document.querySelector("#thread-create-btn").addEventListener("click", () => {
+  goToSubpage("create-thread");
+});
+document.querySelector("#submit-thread-btn").addEventListener("click", (e) => {
+  e.preventDefault();
+  const title = document.querySelector("#create-thread-title").value;
+  const content = document.querySelector("#create-thread-content").value;
+  const isPublic = !document.querySelector("#create-thread-private").checked;
+  request(
+    "thread",
+    "POST",
+    {
+      title,
+      isPublic,
+      content,
+    },
+    token
+  )
+    .then(({ id }) => {
+      viewThread(id, getToken());
+    })
+
+    .catch((err) => displayError(err));
+});
+
+document.querySelector("#edit-thread-btn").addEventListener("click", () => {
+  const title = document.querySelector(".view-thread-title").innerText;
+  const content = document.querySelector(".view-thread-content").innerText;
+  const threadId = document.querySelector(".view-thread-id").innerText;
+
+  request(`thread?id=${threadId}`, "GET", "", token).then(({ isPublic }) => {
+    document.querySelector("#edit-thread-title").value = title;
+    document.querySelector("#edit-thread-content").value = content;
+    document.querySelector("#edit-thread-private").checked = !isPublic;
+
+    goToSubpage("edit-thread");
+  });
+});
+document
+  .querySelector("#edit-submit-thread-btn")
+  .addEventListener("click", (e) => {
+    e.preventDefault();
+    const title = document.querySelector("#edit-thread-title").value;
+    const content = document.querySelector("#edit-thread-content").value;
+    const isPublic = !document.querySelector("#edit-thread-private").checked;
+    const threadId = document.querySelector(".view-thread-id").innerText;
+
+    request(
+      "thread",
+      "PUT",
+      { id: threadId, title, isPublic, content },
+      token
+    ).then(() => viewThread(threadId, getUserId()));
+  });
+
+document.querySelector("#delete-thread-btn").addEventListener("click", () => {
+  const threadId = document.querySelector(".view-thread-id").innerText;
+  request("thread", "DELETE", { id: threadId }, token).then(() =>
+    displayThreads(0).then((data) => {
+      const threadList = document.querySelector("#thread-list");
+      const threadToView = threadList.querySelector(".card-id")?.innerText;
+      if (!threadToView) return goToPage("dashboard");
+      viewThread(threadToView, getUserId());
+    })
+  );
+});
+document.querySelector("#like-thread-btn").addEventListener("click", (e) => {
+  const threadId = document.querySelector(".view-thread-id").innerText;
+  const turnon = !(
+    document.querySelector("#like-thread-btn").style.background === "red"
+  );
+
+  request(
+    "thread/like",
+    "PUT",
+    {
+      id: threadId,
+      turnon,
+    },
+    token
+  ).then(() => viewThread(threadId, getUserId()));
+});
+
+document.querySelector("#watch-thread-btn").addEventListener("click", () => {
+  const turnon = !(
+    document.querySelector("#watch-thread-btn").style.background === "red"
+  );
+  const threadId = document.querySelector(".view-thread-id").innerText;
+  request("thread/watch", "PUT", { id: threadId, turnon }, token).then(() =>
+    viewThread(threadId, getUserId())
+  );
+});
+const container = document.querySelector("#thread-list");
+
+document.querySelector("#next-button").addEventListener("click", () => {
+  displayThreads(page + 5).then((msg) => {
+    msg === "Success" ? (page = page + 5) : displayThreads(page);
+  });
+});
+
+document.querySelector("#prev-button").addEventListener("click", () => {
+  if (page >= 5)
+    displayThreads(page - 5).then((msg) => {
+      msg === "Success" ? (page = page - 5) : displayThreads(page);
+    });
+});
